3ed8c03c066e24846a61a2db176374e27675129c
seoAreum <1971236@hansung.ac.kr>
2024-09-15 23:13:27 +0900

    init: 이모션 색상 세팅

---
+++ src/styles/emotion.d.ts
@@ -0,0 +1,19 @@
+import '@emotion/react';
+
+declare module '@emotion/react' {
+  export interface Theme {
+    colors: {
+      white: string;
+      gray100: string;
+      gray200: string;
+      gray300: string;
+      gray500: string;
+      gray900: string;
+      pink: string;
+      pink300: string;
+      gray_graphic: string;
+      blue: string;
+      orange: string;
+    };
+  }
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  white: '#FFFFFF',
+  gray100: '#EDEDED',
+  gray200: '#DBDBDB',
+  gray300: '#CFCCCC',
+  gray500: '#888485',
+  gray900: '#1F1B1C',
+  pink: '#FABEE8',
+  pink300: '#F293D9',
+  gray_graphic: '#231F20',
+  blue: '#264AC2',
+  orange: '#FF885F',
+};
+
