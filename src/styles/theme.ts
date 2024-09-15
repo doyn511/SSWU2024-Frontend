46ebef65ca7e839b147d9516c2448b48cdbdf318
seoAreum <1971236@hansung.ac.kr>
2024-09-15 23:50:23 +0900

    init: 이모션 폰트 세팅

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,5 @@
+import { css } from '@emotion/react';
+
 export const colors = {
   white: '#FFFFFF',
   gray100: '#EDEDED',
@@ -12,3 +14,144 @@ export const colors = {
   orange: '#FF885F',
 };
 
+export const fonts = {
+  desktop_title_reg_60: css`
+    font-weight: 600;
+    font-size: 6rem;
+    line-height: 140%;
+    letter-spacing: -1%;
+  `,
+  desktop_title_reg_54: css`
+    font-weight: 400;
+    font-size: 5.4rem;
+    line-height: 100%;
+    letter-spacing: -1%;
+  `,
+  desktop_title_semi_40: css`
+    font-weight: 600;
+    font-size: 4rem;
+    line-height: 140%;
+    letter-spacing: -1%;
+  `,
+  desktop_title_semi_28: css`
+    font-weight: 600;
+    font-size: 2.8rem;
+    line-height: 120%;
+    letter-spacing: -1%;
+  `,
+  desktop_title_semi_24: css`
+    font-weight: 600;
+    font-size: 2.4rem;
+    line-height: 100%;
+    letter-spacing: 0%;
+  `,
+  desktop_title_reg_24: css`
+    font-weight: 400;
+    font-size: 2.4rem;
+    line-height: 100%;
+    letter-spacing: 0%;
+  `,
+
+  desktop_body_semi_20: css`
+    font-weight: 600;
+    font-size: 2rem;
+    line-height: 120%;
+    letter-spacing: 0%;
+  `,
+  desktop_body_reg_20: css`
+    font-weight: 400;
+    font-size: 2rem;
+    line-height: 120%;
+    letter-spacing: 0%;
+  `,
+  desktop_body_semi_18: css`
+    font-weight: 600;
+    font-size: 1.8rem;
+    line-height: 150%;
+    letter-spacing: 0%;
+  `,
+  // 줄글
+  desktop_body_reg_18_desc: css`
+    font-weight: 400;
+    font-size: 1.8rem;
+    line-height: 150%;
+    letter-spacing: 0%;
+  `,
+  desktop_body_reg_18: css`
+    font-weight: 400;
+    font-size: 1.8rem;
+    line-height: 120%;
+    letter-spacing: 0%;
+  `,
+  desktop_body_reg_16: css`
+    font-weight: 400;
+    font-size: 1.6rem;
+    line-height: 150%;
+    letter-spacing: 0%;
+  `,
+  desktop_body_lig_16: css`
+    font-weight: 300;
+    font-size: 1.6rem;
+    line-height: 150%;
+    letter-spacing: 0%;
+  `,
+
+  desktop_cap_reg_14: css`
+    font-weight: 400;
+    font-size: 1.4rem;
+    line-height: 100%;
+    letter-spacing: 0%;
+  `,
+
+  // mobile
+  mobile_title_reg_32: css`
+    font-weight: 400;
+    font-size: 3.2rem;
+    line-height: 100%;
+    letter-spacing: -1%;
+  `,
+  mobile_title_reg_20: css`
+    font-weight: 600;
+    font-size: 2rem;
+    line-height: 120%;
+    letter-spacing: -1%;
+  `,
+  mobile_title_med_18: css`
+    font-weight: 500;
+    font-size: 1.8rem;
+    line-height: 100%;
+    letter-spacing: 0%;
+  `,
+  mobile_title_reg_18: css`
+    font-weight: 400;
+    font-size: 1.8rem;
+    line-height: 100%;
+    letter-spacing: 0%;
+  `,
+
+  mobile_body_semi_14: css`
+    font-weight: 600;
+    font-size: 1.4rem;
+    line-height: 120%;
+    letter-spacing: 0%;
+  `,
+  mobile_body_reg_14: css`
+    font-weight: 400;
+    font-size: 1.4rem;
+    line-height: 150%;
+    letter-spacing: 0%;
+  `,
+  mobile_body_lig_14: css`
+    font-weight: 300;
+    font-size: 1.4rem;
+    line-height: 150%;
+    letter-spacing: 0%;
+  `,
+
+  mobile_cap_reg_12: css`
+    font-weight: 400;
+    font-size: 1.2rem;
+    line-height: 120%;
+    letter-spacing: 0%;
+  `,
+};
