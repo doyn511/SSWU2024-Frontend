fec6c0e087fbcc6555453a669902138a07ea0003
seoAreum <1971236@hansung.ac.kr>
2024-09-15 23:13:08 +0900

    init: react-query 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import Router from './Router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
+import MobileRouter from './MobileRouter';
 import { GlobalStyles } from './styles/GlobalStyles';
 
 function App() {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyles />
-      <Router />
-    </>
+      <MobileRouter />
+    </QueryClientProvider>
   );
 }
 
