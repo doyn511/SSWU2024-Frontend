dedd04575d5750e9a4999aeb1c2baefac133142a
seoAreum <1971236@hansung.ac.kr>
2024-09-16 19:11:07 +0900

    style: 에셋 추가

--- src/desktop/assets/image/index.ts
+++ src/desktop/assets/image/index.ts
@@ -3,3 +3,10 @@
 // ex) <img src={Sample01Image} /> 이런 식!
 
 export { default as ImgBg2Web } from './img_bg2_web.svg?react';
+export { default as ImgSymbolDesignThinking } from './img_symbol_design_thinking.svg?react';
+export { default as ImgSymbolInformation } from './img_symbol_informaion.svg?react';
+export { default as ImgSymbolMotion } from './img_symbol_motion.svg?react';
+export { default as ImgSymbolSpace } from './img_symbol_space.svg?react';
+export { default as ImgSymbolSpaceDirection } from './img_symbol_space_direction.svg?react';
+export { default as ImgSymbolStudioA } from './img_symbol_studio_A.svg?react';
+export { default as ImgSymbolStudioB } from './img_symbol_studio_B.svg?react';
