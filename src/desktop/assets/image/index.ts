9d9a928783f696a9d69d920664f279e99618f5b4
seoAreum <1971236@hansung.ac.kr>
2024-09-18 00:19:23 +0900

    style: svg -> png로 변경

--- src/desktop/assets/image/index.ts
+++ src/desktop/assets/image/index.ts
@@ -2,11 +2,11 @@
 // img태그의 src 주소에 Sample01Image 넣으면 됨
 // ex) <img src={Sample01Image} /> 이런 식!
 
-export { default as ImgBg2Web } from './img_bg2_web.svg?react';
-export { default as ImgSymbolDesignThinking } from './img_symbol_design_thinking.svg?react';
-export { default as ImgSymbolInformation } from './img_symbol_informaion.svg?react';
-export { default as ImgSymbolMotion } from './img_symbol_motion.svg?react';
-export { default as ImgSymbolSpace } from './img_symbol_space.svg?react';
-export { default as ImgSymbolSpaceDirection } from './img_symbol_space_direction.svg?react';
-export { default as ImgSymbolStudioA } from './img_symbol_studio_A.svg?react';
-export { default as ImgSymbolStudioB } from './img_symbol_studio_B.svg?react';
+export { default as ImgBg2Web } from './img_bg2_web.png';
+export { default as ImgSymbolDesignThinking } from './img_symbol_design_thinking.png';
+export { default as ImgSymbolInformation } from './img_symbol_informaion.png';
+export { default as ImgSymbolMotion } from './img_symbol_motion.png';
+export { default as ImgSymbolSpace } from './img_symbol_space.png';
+export { default as ImgSymbolSpaceDirection } from './img_symbol_space_direction.png';
+export { default as ImgSymbolStudioA } from './img_symbol_studio_A.png';
+export { default as ImgSymbolStudioB } from './img_symbol_studio_B.png';
