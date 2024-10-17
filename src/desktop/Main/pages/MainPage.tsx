bc00f80880d8bd0fd5becb7d3d896d7611ccc267
doyn511 <doyn511@ajou.ac.kr>
2024-10-17 03:32:15 +0000

    feat: 메인 gif 이미지 로딩 처리

--- src/desktop/Main/pages/MainPage.tsx
+++ src/desktop/Main/pages/MainPage.tsx
@@ -1,15 +1,26 @@
 import { css } from '@emotion/react';
+import { useRef } from 'react';
 import { ImgMotionPoster } from '../../assets/image';
 import PageLayout from '../../Common/PageLayout';
 import InteractiveViews from '../components/InteractiveViews';
 import PreparatoryCommittee from '../components/PreparatoryCommittee';
 
 function MainPage() {
+  const isLoaded = useRef(false);
+
   return (
     <PageLayout>
-      <img src={ImgMotionPoster} css={motionPoster} />
-      <InteractiveViews />
-      <PreparatoryCommittee />
+      <img
+        src={ImgMotionPoster}
+        css={motionPoster}
+        onLoad={() => (isLoaded.current = true)}
+      />
+      {isLoaded.current && (
+        <>
+          <InteractiveViews />
+          <PreparatoryCommittee />
+        </>
+      )}
     </PageLayout>
   );
 }
