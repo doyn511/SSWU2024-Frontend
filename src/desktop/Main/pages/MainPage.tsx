4f3488b06802eb898c5744badfee9ed85faa4232
doyn511 <doyn511@ajou.ac.kr>
2024-10-17 03:38:11 +0000

    fix: 데탑 메인 로딩 삭제

--- src/desktop/Main/pages/MainPage.tsx
+++ src/desktop/Main/pages/MainPage.tsx
@@ -1,26 +1,16 @@
 import { css } from '@emotion/react';
-import { useRef } from 'react';
 import { ImgMotionPoster } from '../../assets/image';
 import PageLayout from '../../Common/PageLayout';
 import InteractiveViews from '../components/InteractiveViews';
 import PreparatoryCommittee from '../components/PreparatoryCommittee';
 
 function MainPage() {
-  const isLoaded = useRef(false);
-
   return (
     <PageLayout>
-      <img
-        src={ImgMotionPoster}
-        css={motionPoster}
-        onLoad={() => (isLoaded.current = true)}
-      />
-      {isLoaded.current && (
-        <>
-          <InteractiveViews />
-          <PreparatoryCommittee />
-        </>
-      )}
+      <img src={ImgMotionPoster} css={motionPoster} />
+
+      <InteractiveViews />
+      <PreparatoryCommittee />
     </PageLayout>
   );
 }
