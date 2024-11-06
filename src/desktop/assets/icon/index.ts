305cebfcbe5acc1f788661b32c6696f6ff4d2c3a
doyn511 <doyn511@ajou.ac.kr>
2024-11-06 04:45:17 +0000

    chore: 사용하지 않는 svg 삭제

--- src/desktop/assets/icon/index.ts
+++ src/desktop/assets/icon/index.ts
@@ -1,5 +1,4 @@
 /// <reference types="vite-plugin-svgr/client" />
-export { default as IcCircle } from './ic_circle.svg?react';
 export { default as IcGnbGraphic } from './ic_gnbgraphic.svg?react';
 export { default as IcInsta } from './ic_insta.svg?react';
 export { default as IcListRight } from './ic_listright.svg?react';
